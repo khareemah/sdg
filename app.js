d9ddae936239f19afb1425c2928d4615c7ac42d6
khareemah <ajimobikareemah@gmail.com>
2021-03-07 16:18:50 -0800

    add the faq section

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+// nav toggle
 const navToggle = document.querySelector(".nav-toggle span");
 const topNav = document.querySelector(".header .topnav");
 navToggle.addEventListener("click", function() {
   topNav.classList.toggle("open");
+});
+
+// fixed navbar
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset > 200) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+// faq
+const questions = document.querySelectorAll(".question-card");
+questions.forEach(question => {
+  const questionBtn = question.querySelector(".question-btn");
+  questionBtn.addEventListener("click", function() {
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 });
