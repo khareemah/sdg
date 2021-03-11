9076a50625a292d11179b91fda617dec8a3696f2
khareemah <ajimobikareemah@gmail.com>
2021-03-10 20:38:39 -0800

    add contact, footer and our mission section

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 // nav toggle
 const navToggle = document.querySelector(".nav-toggle span");
 const topNav = document.querySelector(".header .topnav");
+const ul = topNav.querySelector("ul");
 const navLinks = topNav.querySelectorAll("ul li");
 
+// toggle nav on small device
 navToggle.addEventListener("click", function() {
-  topNav.classList.toggle("open");
+  ulHeight = ul.getBoundingClientRect().height;
+  topNavHeight = topNav.getBoundingClientRect().height;
+  if (topNavHeight == 0) {
+    topNav.style.height = `${ulHeight}px`;
+  } else {
+    topNav.style.height = 0;
+  }
 });
 
+// set scroll links to hrefs
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(scrollLink => {
+  scrollLink.addEventListener("click", function(e) {
+    e.preventDefault();
+    const href = scrollLink.getAttribute("href").slice(1);
+    const target = document.getElementById(href);
+    const topNavHeight = topNav.getBoundingClientRect().height;
+
+    const headerHeight = document
+      .querySelector(".header")
+      .getBoundingClientRect().height;
+    let position = target.offsetTop - headerHeight;
+    if (headerHeight > 82) {
+      position = position + topNavHeight;
+    }
+    window.scrollTo({
+      top: position,
+      left: 0
+    });
+    topNav.style.height = 0;
+  });
+});
 // close navbar when link is clicked on mobile
 navLinks.forEach(navLink => {
   navLink.addEventListener("click", function() {
@@ -27,3 +58,7 @@ questions.forEach(question => {
     }
   });
 });
+
+// footer date
+const year = document.querySelector("footer .year");
+year.innerHTML = new Date().getFullYear();
